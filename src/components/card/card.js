d1a5593998b6bc6648ae284382a78ecf42d980c7
Semolik <70706334+Semolik@users.noreply.github.com>
2022-01-09 15:11:14 +0700

    upd

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -5,7 +5,12 @@ function Card(item){
     var data = item.data;
     return (
         <div className="card">
-            <img src={data.poster} alt={data.ru_title}></img>
+            <div className="poster-container">
+                <img src={data.poster} alt={data.ru_title}></img>
+                <div className="poster">
+
+                </div>
+            </div>
             <div className="title">{data.ru_title}</div>
         </div>
     )
