5cb0bbfe0e46877290e411dc18381f9264b9f00b
Semolik <70706334+Semolik@users.noreply.github.com>
2022-01-11 23:37:41 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,47 +17,81 @@ class ServicePage extends React.Component {
 			id: this.props.match.params.id,
 			service: this.props.match.params.service,
 			api: `http://127.0.0.1/api/${this.props.match.params.service}/`,
+			page_type: '',
 		};
 	}
   
 	componentDidMount() {
-		if (this.state.page===undefined| this.state.page==='page'){
-		fetch(this.state.api,{
-			method: 'post',
-			headers: {
-				'Accept': 'application/json, text/plain, */*',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify( {page: this.state.id} ),
-		})
-		.then(res => res.json())
-		.then(
-			(result) => {
-				console.log(result);
-				if (result.status===200){
-					this.setState({
-						isLoaded: true,
-						data: result.data
-					});
-				} else {
+		if (this.state.page===undefined | this.state.page==='page'){
+			fetch(this.state.api,{
+				method: 'post',
+				headers: {
+					'Accept': 'application/json, text/plain, */*',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify( {page: this.state.id} ),
+			})
+			.then(res => res.json())
+			.then(
+				(result) => {
+					console.log(result);
+					if (result.status===200){
+						this.setState({
+							isLoaded: true,
+							data: result.data,
+							page_type: 'page',
+						});
+					} else {
+						this.setState({
+							isLoaded: false,
+							error: {
+								message: result.message
+							}
+						});
+					}
+				},
+				(error) => {
 					this.setState({
 						isLoaded: false,
-						error: {
-							message: result.message
-						}
-					});
-				}
-			},
-			(error) => {
-				console.log(3);
-				this.setState({
-					isLoaded: false,
-					error
-					});
-				}
-		)
+						error
+						});
+					}
+			)
 		} else if (this.state.page!==undefined){
-			//Обработка страницы отдельного аниме
+			fetch(this.state.api+'title',{
+				method: 'post',
+				headers: {
+					'Accept': 'application/json, text/plain, */*',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify( {id: this.state.page} ),
+			})
+			.then(res => res.json())
+			.then(
+				(result) => {
+					console.log(result);
+					if (result.status===200){
+						this.setState({
+							isLoaded: true,
+							data: result.data,
+							page_type: 'title',
+						});
+					} else {
+						this.setState({
+							isLoaded: false,
+							error: {
+								message: result.message
+							}
+						});
+					}
+				},
+				(error) => {
+					this.setState({
+						isLoaded: false,
+						error
+						});
+					}
+			)
 		} else {
 			this.setState({
 				isLoaded: true,
@@ -66,15 +100,15 @@ class ServicePage extends React.Component {
 				}
 			});
 		}
-	}
+	} 
 	render() {
-		const { error, isLoaded, data, service, id} = this.state;
+		const { error, isLoaded, data, service, id, page_type} = this.state;
 		if (error) {
 			return <div>Ошибка: {error.message}</div>;
 		} else if (!isLoaded) {
 			return <Loading/>;
-		} else {
-			console.log(data);
+		} else if (page_type=='page'){
+			
 			return (
 				<div className='wrapper'>
 					<div className='cards-container'>
@@ -86,9 +120,17 @@ class ServicePage extends React.Component {
 					</div>
 					<Pagination totalPages={data.pages} page={id===undefined ? 1 : id} url={`/${service}/page/`}/>
 				</div>
+			)		
+		} else if (page_type=='title'){
+			console.log(data);
+			return (
+				<div className='wrapper'>
+					<div className='title-container'>
+						
+					</div>
+				</div>
 			)
-				
-	  }
+		}
 	}
 }
 export default ServicePage;
