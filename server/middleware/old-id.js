3fc97095c7a99cee0062c9e03f4c3a856ca11108
Semolik <70706334+Semolik@users.noreply.github.com>
2024-08-26 16:59:47 +0700

    Update old-id.js

--- server/middleware/old-id.js
+++ server/middleware/old-id.js
@@ -7,11 +7,18 @@ ParsersService.getParsersApiV1ParsersGet().then((data) => {
 export default defineEventHandler(async (event) => {
     const parts = event.path.split("/");
     if (parts.length > 2 && parsers.find((parser) => parser.id === parts[1])) {
-        const title_id =
-            await ParsersService.resolveOldIdApiV1ParsersParserIdResolveOldIdTitleIdGet(
-                parts[1],
-                parts[2].split("~")[1].split("-")[0]
-            );
-        return sendRedirect(event, `/titles/${title_id}`, 301);
+        try {
+            const id = !isNaN(parts[2])
+                ? parts[2]
+                : parts[2].split("~")[1].split("-")[0];
+            const title_id =
+                await ParsersService.resolveOldIdApiV1ParsersParserIdResolveOldIdTitleIdGet(
+                    parts[1],
+                    id
+                );
+            return sendRedirect(event, `/titles/${title_id}`, 301);
+        } catch (e) {
+            return sendRedirect(event, `/parsers/${parts[1]}`);
+        }
     }
 });
